7f345bd5cabca9e0704f433baf3a1637b0e00238
cassandrale179 <lenguyetminh1998@gmail.com>
2020-03-24 19:09:12 -0400

    TA schedule

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,63 +2,72 @@
   <div>
 
   <div class="sidebar card">
-    <div class="sidebar-title card">
-      <!-- <h1> Cyber Learning Center </h1>
-      <h3> Drexel University Department of Computer Science </h3>
-      <h5> Market Room 1066 &#8231; Winter 2020 </h5> -->
+    <div class="sidebar-title card">  
+       <h1> Cyber Learning Center </h1> 
     </div>
     <div class="classes">
-      <!-- <div class="course"> CS 171: Intro to Programming I <span class="arrow"> >> </span> </div>
-      <div class="course"> CS 172: Intro to Programming II <span class="arrow"> >> </span>  </div>
-      <div class="course"> SE 181: Software Engineering <span class="arrow"> >> </span>  </div>
-      <div class="course"> CS 360: Programming Languages <span class="arrow"> >> </span>  </div> -->
+    
     </div>
   </div>
 
   <div class="mainview">
     <div class="mainview-title card">
-
+       
     </div> 
-     <!-- <h3> Drexel University Department of Computer Science </h3>
-      <h5> Market Room 1066 &#8231; Winter 2020 </h5> -->
-    <!-- <div class="timetable">
+    
+     <div class="courses"> 
+       <h4> Choose a class on the sidebar to see TA availables and their schedules </h4>
+     </div>
+
+    <div class="timetable">
     <table>
       <tr>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
+        <th>Time</th>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
       </tr>
       <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
+        <td>10:00 AM</td>
+        <td>
+          <ul> 
+            <li> Giang Doan </li>
+            <li> John Greaves </li>
+            <li> Kevin Young </li>
+           </ul> 
+        </td> 
+      
       </tr>
       <tr>
-        <td>Centro comercial Moctezuma</td>
+         <td>11:00 AM</td>
         <td>Francisco Chang</td>
         <td>Mexico</td>
       </tr>
       <tr>
-        <td>Ernst Handel</td>
+        <td>12:00 PM</td>
         <td>Roland Mendel</td>
         <td>Austria</td>
       </tr>
       <tr>
-        <td>Island Trading</td>
+         <td>1:00 PM</td>
         <td>Helen Bennett</td>
         <td>UK</td>
       </tr>
       <tr>
-        <td>Laughing Bacchus Winecellars</td>
+         <td>2:00 PM</td>
         <td>Yoshi Tannamuri</td>
         <td>Canada</td>
       </tr>
       <tr>
-        <td>Magazzini Alimentari Riuniti</td>
+        <td>3:00 PM</td>
         <td>Giovanni Rovelli</td>
         <td>Italy</td>
       </tr>
-    </table> </div> -->
+    </table> 
+    
+    </div>
   </div>
 </div>
 
@@ -70,9 +79,22 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String, 
+  }, 
+  data(){
+    return {
+      items: [ 'CI 102', 'CI 103', 'CI 492', 'CS 164', 
+      'CS 171', 'CS 172', 'CS 260', 'CS 265', 'CS 270', 'CS 283', 
+      'CS 303', 'CS 338', 'CS 342', 'CS 345', 'CS 360', 'CS 370', 
+      'CS 375', 'CS 383', 'CS 387', 'CS 432', 'CS 435', 'CS 522', 'CS 525', 
+      'CS 530', 'CS 544', 'CS 571', 'CS 613', 'CS 645', 'CS 676', 
+      'CS T680', 'SE 181',' SE 211', 'SE 310', 'SE 311', 'SE 410', 'SE 576', 'SE 577'] 
+    }
   }
 }
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -98,21 +120,36 @@ hr {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 }
 
-.course{
-  margin-top: 1vh; 
-  padding: 1vh; 
+.courses{
+  width: 90%; 
+  margin-top: -30px; 
 }
+.course{
+  padding-left: 2vh; 
+  padding-right: 2vh; 
+  background: #212F3D; 
+  display: inline; 
+  line-height: 3em; 
+  color: white; 
+  border-radius: 5px; 
+  font-size: 10px; 
+  font-family: 'Raleway', sans-serif; 
 
+}
+ 
 .sidebar{
   position: absolute;
   height: 100%; 
   width: 40vh; 
-  background: #34495E; 
+  background: #5D6D7E; 
   color: white; 
+  z-index: 1000;
 }
 
-.sidebar h3, h5{
-  margin-top: -10px;  
+.sidebar h1{
+  margin-top: 13px;  
+  font-size: 20px; 
+  text-align: center;
 }
 
 .sidebar hr{
@@ -120,8 +157,9 @@ hr {
 }
 
 .sidebar-title{
-  height: 10vh; 
-  background: #2E4053; 
+  height: 6vh; 
+  background: #34495E; 
+  padding: 2vh; 
 }
 
 .mainview{
@@ -131,6 +169,8 @@ hr {
   right: 0px; 
   bottom: 0px; 
   background: #F8F9F9; 
+  padding-left: 10vh; 
+  padding-top: 20vh; 
 }
 
 .mainview-title{
@@ -143,16 +183,23 @@ hr {
  
 }
 
-
-
 .timetable{
-  margin-left: 5vh; 
-  margin-top: 5vh; 
   width: 100vh; 
   height: 80vh; 
 }
 
-
+th{
+  background: #808B96; 
+  color: white; 
+  width: 25vh; 
+  height: 5vh; 
+}
+td{
+  width: 25vh; 
+  height: 5vh; 
+  background: white;
+  padding: 1vh;  
+}
 
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faCoffee)
 
-// Vue.component('font-awesome-icon', FontAwesomeIcon) 
-
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
-// new Vue({
-//   el: "#app",
-//   components: { App },
-//   template: "<App/>"
-// });
-
-
 
 new Vue({
   render: h => h(App),
